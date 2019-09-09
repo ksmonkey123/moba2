04c4046aca183f961abb7aada44e2b67e4b86a8c
Andreas Wälchli <andreas.waelchli@me.com>
2019-09-09 17:20:53 +0200

    continued implementation

---
+++ src/main/java/ch/awae/moba2/PathRegistry.java
@@ -0,0 +1,61 @@
+package ch.awae.moba2;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PathRegistry {
+
+    private ArrayList<Path> paths = new ArrayList<>();
+
+    public void register(Path... paths) {
+        for (Path path : paths) {
+            register(path);
+        }
+    }
+
+    public void register(Path path) {
+        if (this.paths.contains(path)) {
+            return;
+        }
+        for (int index = 0; index < this.paths.size(); index++) {
+            Path p = this.paths.get(index);
+            if (path.collides(p)) {
+                this.paths.remove(index);
+                index--;
+            }
+        }
+        this.paths.add(path);
+    }
+
+    public List<Path> getAllPaths() {
+        return new ArrayList<>(paths);
+    }
+
+    public void unregister(Path... paths) {
+        for (Path path : paths) {
+            unregister(path);
+        }
+    }
+
+    public void unregister(Path path) {
+      this.paths.remove(path);
+    }
+
+    public List<Path> getPaths(Sector sector) {
+        List<Path> result = new ArrayList<>();
+        for (Path p : this.paths)
+            if (p.sector == sector)
+                result.add(p);
+        return result;
+    }
+
+    public boolean isRegistered(Path p) {
+        if (p == null)
+            return false;
+        return this.paths.contains(p);
+    }
+
+}
