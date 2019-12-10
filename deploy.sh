91c41887e4d165d727b080401adde844ce083145
Andreas Wälchli <andreas.waelchli@me.com>
2019-12-10 22:32:02 +0100

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 echo "running maven build..."
 mvn -U clean package
-echo "Done!"
+echo "done!"
 
 echo "installing on host..."
 cd target
 scp *.jar othala:~/moba-core.jar
-
-echo "Done!"
+echo "done!"
+echo
+echo "restarting host... (Ctrl-C to abort)"
+ssh -t othala 'sudo reboot'
